9c10cd9b316629ffd53e39031d80f8709bd654e4
Nate Cottle and Ryan Putman <ryan@>
2018-06-12 10:27:35 -0700

    Add backend logic for movie page, styling.

--- README.md
+++ README.md
@@ -15,6 +15,20 @@
 1. Commit and push your changes
 1. Create a PR
 
+## Specifications
+
+* Users can select the name of the movie, the time of day they want to see it, and their age:
+  * Example Input: "Movie", "Showtime", "Age"
+  * Example Output: Avengers, 7:00pm, 18
+
+* Depending on what the user selects their ticket price is calculated
+  * Example Input: Avengers, 7:00pm, 18
+  * Example Output: $11
+
+* Ticket price will be adjusted based on movie release date and other discounts
+  * Example Input: $11
+  * Example Output: Senior Discount - $9
+
 ## Technologies Used
 
 * Bootstrap x.x.x
@@ -22,7 +36,7 @@
 
 ## Links
 
-* 
+*
 
 ## License
 
---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+// Business Logic
+function Ticket(movie, showtime, age) {
+  this.movie = movie;
+  this.showtime = showtime;
+  this.age = age;
+}
+
+function ticketPrice (input) {
+  var price = 5
+  if (input.movie === "Movie 1" || input.movie === "Movie 3") {
+    price += 3
+  } else {
+    price += 2
+  }
+  if (input.showtime === "Showtime 4" || input.showtime === "Showtime 5" || input.showtime === "Showtime 6" ) {
+    price += 3
+  } else {
+    price += 2
+  }
+  if (input.age >= 65 ) {
+    price += 1
+  } else {
+    price += 3
+  }
+  return price
+}
+// User Interface Logic
+
+$(document).ready(function() {
+
+  $("#movies span").click(function() {
+    var movie = $(this).text();
+    $("#movies span").not(this).slideToggle();
+    $("#showtime").slideToggle("slow");
+
+    $("#showtime span").click(function() {
+      var showtime = $(this).text();
+
+      $("#ageInput").slideToggle("slow");
+
+
+    $("#ageInput button").click(function(event) {
+      event.preventDefault();
+      var age = parseInt($("input#age").val());
+      var ticket = new Ticket (movie, showtime, age)
+      $(".ticket-price h1").text(ticketPrice(ticket))
+      });
+    });
+  });
+
+
+
+
+});
